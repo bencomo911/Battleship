b6fb97e24a8e8f7ee377837a9355d5a68fc3d31a
Dafne Bencomo <bencomo911@gmail.com>
2020-01-28 08:59:46 -0700

    Update BattleShip.java
    try catch working

--- src/Battleship/BattleShip.java
+++ src/Battleship/BattleShip.java
@@ -3,16 +3,38 @@ import java.util.Scanner;
 
 public class BattleShip {
 	static int n = 9;//used to create nxn grid
+	static int specified_row;
+	static int specified_column;
 	static boolean cornerCell = false;
 	static boolean first_Row = false;
 	static boolean last_Row = false;
 	static boolean first_Column = false;
 	static boolean last_Column = false;
 	static int [][] existing_squares = new int [n][n]; 
+	 
+	public static void create_grid() { //scans user input
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter current coordinate separated by a space: ");
+		
+
+		String[] coordinate = sc.nextLine().split(" "); //Create array with given coordinates
 	
+		specified_row =  Integer.parseInt(coordinate[0]); //Convert input to integer
+		specified_column = Integer.parseInt(coordinate[1]);
+
+		sc.close();
+		
+
+	}
 	
-	public static void populate_squares(int x, int y) { //adds touched cells into grid
-		existing_squares[x][y] = -1;
+	public static void populate_squares(int x, int y) { //adds adjacent cells into grid
+		try {
+			existing_squares[x][y] = -1;
+		}catch(ArrayIndexOutOfBoundsException e) {
+			
+			System.out.println("Index out of bounds, please restart program and try again");
+			System.exit(0);
+		}
 	}
 	
 	
@@ -39,6 +61,7 @@ public class BattleShip {
 	}
 	
 	/*********************** Data-Printing Methods ******************************/
+	//the following methods are used to print the specified cells and add them to the global variable existing_squares
 	public static void top_neighbor(int x, int y){
 		x = x+1;
 		System.out.println("[" + (x) + "," + (y) + "]" );
@@ -102,9 +125,10 @@ public class BattleShip {
 		for(int row = 0; row < n; row++) {
 			for(int column = 0; column < n; column++) {
 				if(grid[row][column] != -1) {
-					System.out.println("[" + (row) + "," + (column) + "]");
+					System.out.print("[" + (row) + "," + (column) + "] ");
 				}
 			}
+			System.out.println();
 		}
 		
 	}
@@ -261,68 +285,20 @@ public class BattleShip {
 		
 	}
 	
-	public static void non_adjacent_special_cases(int x, int y) {
-		
-		/*************** corner edge-cases *******************/
-		if(cornerCell == true) { //corner cases
-			if(x == 0 && y == 0) {
-				int constant = 2;
-				for(int i = 0; i < 9; i++ ) {
-					System.out.println();
-				}
-			}
-			
-			else if(x == 0 && y == n-1) {
-				top_left_diagonal(x,y);
-			}
-			
-			else if(x == n-1 && y == 0) {
-				bottom_right_diagonal(x,y);
-			}
-			
-			else if(x == n-1 && y == n-1) {
-				bottom_left_diagonal(x,y);
-			}
-		}
-		/*****************************************************/
-		
-	}
-	
-	public static void non_adjacent_squares(int x, int y) { //takes care of non-adjacent squares
-		
-		if(cornerCell == true) {
-			
-		}
-		
-	}
 
 	public static void main(String [] args) {
 		
-		populate_squares(0, 0);
+		create_grid();
 		
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter current coordinate separated by a space: ");
-		
-		
-		String[] coordinate = sc.nextLine().split(" "); //Create array with given coordinates
-		int row =  Integer.parseInt(coordinate[0]); //Convert input to integer
-		int column = Integer.parseInt(coordinate[1]);
-		sc.close();
-		
+		populate_squares(specified_row, specified_column);
+			
 	
-		cell_detection(row, column);//updates object parameters appropriately 
-		
-//		System.out.println("corner: " + cornerCell);
-//		System.out.println("LastRow: " + last_Row);
-//		System.out.println("firstRow: " + first_Row);
-//		System.out.println("firstCol: " + first_Column);
-//		System.out.println("lastCol: " + last_Column);
-		
+		cell_detection(specified_row, specified_column);//updates object parameters appropriately 
+	
 		System.out.println("\nedge-adjacent squares: ");
-		edges_adjacent_squares(row, column);
+		edges_adjacent_squares(specified_row, specified_column);
 		System.out.println("\ncorner-adjacent squares: ");
-		corners_adjacent_squares(row,column);
+		corners_adjacent_squares(specified_row, specified_column);
 		
 		System.out.println("\nnon-adjacent squares: ");
 		untouched_squares(existing_squares);
